f4553a1540692d1f3f7a7f2a310877a326b2a9d5
Mark Rucker <rucker.mark@gmail.com>
2026-06-24 15:42:08 -0400

    Fixed compliance page.

--- pages/mtm/compliance.py
+++ pages/mtm/compliance.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
 from auth import check_access_admin_only
-from utils import load_data, completed_flow_values, consents_as_flows
+from utils import load_data, completed_flow_values, consents_as_events, invites_as_events
 
 check_access_admin_only()
 
-def index_flow_name(df):
-    start_times = { r.linking_code: r.date for r in compliance_flows.itertuples() if r.flow_name == "consent" }
+def index_typ(df):
+    start_times = { r.linking_code: r.date for r in df.itertuples() if r.event == "invite" }
     for r in df.itertuples():
-        if r.flow_name != "track your progress":
-            yield r.flow_name
+        if r.event != "track your progress":
+            yield r.event
         else:
             start_time = start_times[r.linking_code]
             i = int((r.date-start_time).total_seconds()//(60*60*24*14))
@@ -17,18 +17,26 @@ def index_flow_name(df):
 
 study = st.session_state.get("study")
 data = load_data(study)
-flows = completed_flow_values(data)[["flow_name","linking_code","date"]].drop_duplicates()
-consents = consents_as_flows(data)
 
-is_intro = flows['flow_name'] == "intro"
-is_TYP   = flows['flow_name'].str.startswith("track your progress")
+flows = completed_flow_values(data, only_completed=False)\
+    .groupby(["flow_name","flow_id","linking_code"])["date"].min()\
+    .reset_index(drop=False)\
+    .rename(columns={"flow_name": "event"})[["event","linking_code","date"]]
 
-compliance_flows = pd.concat([consents,flows[is_intro|is_TYP]]).sort_values(["linking_code","date","flow_name"])
+invites = pd.concat([consents_as_events(data),invites_as_events(data)])
+invites["event"] = "invite"
 
-compliance_flows["flow_name"] = list(index_flow_name(compliance_flows))
-compliance_flows["date"] = compliance_flows["date"].dt.date
+is_intro = flows['event'] == "intro"
+is_TYP   = flows['event'].str.startswith("track your progress")
 
-compliance_flows = compliance_flows.sort_values(["linking_code","date","flow_name"]).groupby(['linking_code',"flow_name"]).tail(1)
+compliance_events = pd.concat([invites,flows[is_intro|is_TYP]]).sort_values(["linking_code","date","event"])
+
+compliance_events["event"] = list(index_typ(compliance_events))
+compliance_events["date"]  = compliance_events["date"].dt.date
+
+compliance_events = compliance_events.sort_values(["linking_code","date","event"]).groupby(['linking_code',"event"]).tail(1)
+
+column_order = ["invite","intro","track your progress 1","track your progress 2","track your progress 3","track your progress 4"]
 
 st.title("Compliance")
-st.write(compliance_flows.pivot(columns="flow_name",index="linking_code",values="date"))
+st.write(compliance_events.pivot(columns="event",index="linking_code",values="date")[column_order])

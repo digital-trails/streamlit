9039d0c6797f5e3fd9c6ee4f2457a14a8bfd6844
Mark Rucker <rucker.mark@gmail.com>
2026-07-09 17:48:57 -0400

    Fixed flows.

--- pages/surveys.py
+++ pages/surveys.py
@@ -16,6 +16,12 @@ if flows is None or flows.empty:
     st.text("No data is available")
 
 else:
+
+    flow_dates = flows.groupby("flow_id")["date"].min().reset_index()
+    flow_dates["date"] = flow_dates["date"].dt.date
+    flows = flows[["linking_code","flow_id","name","value"]]
+    flows = flows.merge(flow_dates)
+
     for fn in flows["flow_name"].drop_duplicates().sort_values().tolist():
         st.write(f"### {fn}")
         flow = flows[flows["flow_name"] == fn]
